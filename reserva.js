c664314a0c1f377eedf2e62d85b60c1ea7f71f2b
juan-zapata-2006 <juanjosezapatamosquera@gmail.com>
2022-03-22 16:48:30 -0500

    se actualiza

--- reserva.js
+++ reserva.js
@@ -1,6 +1,12 @@
 let contenedor = document.getElementById("contenedor");
+let bander = true 
 contenedor.addEventListener("click", function(evento){
     if(evento.target.classList.contains("w-100"))    {
 evento.target.src="img/rojo.png"
+ bandera= false
+    }
+    else if (evento.target.classList.contains("w-100")){
+        evento.target.src="img/rojo.png"
+        bandera= false
     }
 })
