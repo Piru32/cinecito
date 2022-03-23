ee8ecac43ae3648f3f42694b408b33d6b4c206ac
juan-zapata-2006 <juanjosezapatamosquera@gmail.com>
2022-03-23 16:35:26 -0500

    se termina

--- reserva.js
+++ reserva.js
@@ -1,12 +1,36 @@
 let contenedor = document.getElementById("contenedor");
-let bander = true 
+ let bandera = true
 contenedor.addEventListener("click", function(evento){
+ 
     if(evento.target.classList.contains("w-100"))    {
 evento.target.src="img/rojo.png"
- bandera= false
     }
     else if (evento.target.classList.contains("w-100")){
         evento.target.src="img/rojo.png"
         bandera= false
     }
 })
+
+let boton=document.getElementById("boton")
+boton.addEventListener("click",recibirDatos)
+function recibirDatos(event){
+ event.preventDefault();
+
+ let select=document.getElementById("select").value
+ let cantidad=document.getElementById("cantidad").value
+ let selector=document.getElementById("selector").value
+ let boleta= 5000
+ let cantTotal = cantidad * boleta
+
+ if (selector == "Preferencial"){
+  boleta= 6500
+ } else {
+ let boleta = 5500
+}
+
+ console.log(select)
+ console.log(cantidad)
+console.log(selector)
+alert("Su valor a pagar es de " + cantTotal)
+
+}
